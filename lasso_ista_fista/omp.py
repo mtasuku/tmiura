7e485d261ab29d88614cc61038f63c3c645a0b1b
tmiura <t.miura1993@gmail.com>
2016-09-11 20:22:54 +0900

    add matrix.shape

--- lasso_ista_fista/omp.py
+++ lasso_ista_fista/omp.py
@@ -2,34 +2,35 @@
 """
 Created on Tue Sep  6 13:29:36 2016
 
-@author: 高井wing
+@author: tmiura
 """
 
 import numpy as np
-import numpy.random as npr
-
+#%%
+"Orthogonal Matching Pursuit(OMP)"
 def OMP(b, A, k, delta):
-#    x = np.zeros((n, 1))
+    m, n = A.shape
     x = np.zeros(n)
-#    T = set()
+
     T = np.array([], dtype = int)
     r = b
     
     while np.size(T) <= k and np.linalg.norm(r)/np.linalg.norm(b) > delta:
-        c = np.dot(np.transpose(A), r)
+        c = np.dot(A.T, r)
         s = np.argmax(np.abs(c))
-#        T = np.r_[T, s]
+
         T = np.append(T, s)
         x[T] = np.linalg.lstsq(A[:,T],b)[0]
         r = b - np.dot(A, x)
         
     return x
- 
+#%%
+"Test programs"    
 rng = np.random.RandomState(40)
 m, n = 300, 400
 
-# random design
-A = rng.randn(m, n)  # random design
+
+A = rng.randn(m, n)
 
 x0 = rng.randn(n)
 x0[abs(x0) < 2] = 0
@@ -41,7 +42,6 @@ plt.close('all')
 
 plt.figure()
 plt.stem(x0, markerfmt='g.')
-#plt.stem(x_ista, markerfmt='bo')
 plt.stem(x_est, markerfmt='rx')
 
 plt.show()
