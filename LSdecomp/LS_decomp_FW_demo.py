e8295d5d0e92f0f5addcd6cabaf15936ac38cef5
tmiura <t.miura1993@gmail.com>
2017-12-02 07:11:53 +0900

    can't running!!

--- LSdecomp/LS_decomp_FW_demo.py
+++ LSdecomp/LS_decomp_FW_demo.py
@@ -14,28 +14,32 @@ import spmlib.solver.relaxations as relax
 import scipy.sparse.linalg as linalg
 
 
-def ls_decomp_FW(filename, width = 16384, max_nnz_rate = 800 / 262144,
-                 sparsify = 0.01, taps = 10, wl_weight = 0.25, verbose = False, fc = 120):
-    
-    
+def read_file(filename):
     #read
     if os.path.exists(filename + '.wav') == False:
-        Fs, signal  = swf.read(filename + '.WAV')
+        Fs, data  = swf.read(filename + '.WAV')
     else:
-        Fs, signal = swf.read(filename + '.wav')
-        
+        Fs, data = swf.read(filename + '.wav')
     
+    #normalize between -1 and 1, change dtype double
+    data = data.astype(np.float64)
+    data = data // 32768.0
     
-    signal = signal.astype(np.float64)
-    signal = signal // 32768.0
+    return Fs,data
+    
+def run_LSdecompFW(filename, width = 16384, max_nnz_rate = 8000 / 262144,
+                 sparsify = 0.01, taps = 10, wl_weight = 0.25, verbose = False, fc = 120):        
+
+    Fs,signal = read_file(filename)
+    
     length = signal.shape[0]    
     signal = np.concatenate([np.zeros([int(width/2)]), signal[0:length], np.zeros([int(width)])],axis=0)
     n_wav = signal.shape[0]
-    print(n_wav)
-    signal_dct = np.zeros((n_wav, 1))
-    signal_wl = np.zeros((n_wav, 1))
+    print(signal.shape)
+    signal_dct = np.zeros((n_wav, 0),dtype=np.float64)
+    signal_wl = np.zeros((n_wav, 0),dtype=np.float64)
     
-    pwindow = np.zeros(width, dtype = float)
+    pwindow = np.zeros(width, dtype=np.float64)
     for i in range(width // 2):
         pwindow[i] = i
     for i in range(1, width // 2 + 1):
@@ -45,11 +49,9 @@ def ls_decomp_FW(filename, width = 16384, max_nnz_rate = 800 / 262144,
     #if fc > 0:
         #d = fdesign.hipass()
     
-    w_s = 1
+    w_s = 0
     w_e = width
-    c1 = []
-    c2 = []
-    reconst_error = []
+
     START = np.array([n_wav])
     END = np.array([n_wav])
     i = 0
@@ -58,8 +60,9 @@ def ls_decomp_FW(filename, width = 16384, max_nnz_rate = 800 / 262144,
         print('%1.3f - %1.3f [s]\n' % (w_s/Fs, w_e/Fs))
         START[i] = w_s / Fs
         END[i] = w_e / Fs
-        sig_dct, sig_wl, c1[i,1], c2[i,2], reconst_error[i,1] = LSDecompFW(signal=signal[w_s:w_e], 
-                          width = 16384, max_nnz_rate = 8000 / 262144,sparsify = 0.01, taps = 10, wl_weight = 0.25, verbose = False, fc = 120 )
+        sig_dct, sig_wl = LSDecompFW(wav=signal[w_s:w_e], width = 16384, 
+                                         max_nnz_rate = 8000 / 262144, sparsify = 0.01, taps = 10, 
+                                         wl_weight = 0.25, verbose = False, fc = 120 )
         signal_dct[w_s:w_e] = signal_dct[w_s:w_e] + pwindow * sig_dct
         signal_wl[w_s:w_e] = signal_wl[w_s:w_e] + pwindow * sig_wl
         
@@ -78,41 +81,30 @@ def ls_decomp_FW(filename, width = 16384, max_nnz_rate = 800 / 262144,
     swf.write(filename +'_F.wav',Fs,signal_dct)
     swf.write(filename +'_W.wav',Fs,signal_wl)
     
-    print('end of ls_decomp_FW!!')
+    print('end of run_LSdecomp_FW!!')
 
-    
-def nextpow2(n):
-    m_f = np.log2(n)
-    m_i = np.ceil(m_f)
-    return int(np.log2(2**m_i))
 
-def LSDecompFW(signal, width= 2**14,max_nnz_rate=8000/262144, sparsify = 0.01, taps = 10, 
+def LSDecompFW(wav, width= 2**14,max_nnz_rate=8000/262144, sparsify = 0.01, taps = 10, 
                level = 3, wl_weight = 1, verbose = False,fc=120):
     
     MaxiterA = 60
    
-    length = len(signal)
-    
+    length = len(wav)
+    print('len: '+str(length))
     n = sft.next_fast_len(length)
-    
+    print('n: '+str(n))
     signal = np.zeros((n))
-    
-    Phi = lambda c: c
-    PhiT = lambda x: x
-#    
+    signal[0:length] = wav[0:length]
+     
     h0,h1 = daubcqf(taps,'min')
     L = level
     
-    temp = np.concatenate([sft.idct(signal[0:n]),float(wl_weight)* idwt(signal[n+1:2*n],h0,h1,L)[0]],axis=0)
-    temp2 = np.concatenate([sft.dct(signal),float(wl_weight)* dwt(signal,h0,h1,L)[0]],axis=0)
-    print(temp2.shape)
+    temp = sft.idct(signal[0:n]) + (1.0)*(wl_weight)* idwt(signal[0:n],h0,h1,L)[0]
+    temp2 = np.concatenate([sft.dct(signal),(1.0)*(wl_weight)* dwt(signal,h0,h1,L)[0]],axis=0)
+    temp = temp.astype(np.double)
+    temp2 = temp2.astype(np.double)
     print(temp.shape)
-    Psi = lambda c: np.concatenate([sft.idct(signal[0:n]) , float(wl_weight) * idwt(c[n+1:2*n], h0,h1,L)[0]]) 
-    PsiT = lambda x: np.concatenate([sft.dct(x), float(wl_weight) * dwt(x,h0,h1,L)[0]]) 
-#    
-    Theta = lambda c: Phi(Psi(c))
-    ThetaT = lambda y: PsiT(PhiT(y))
-    
+    print(temp2.shape)
     #measurment
     
 
@@ -120,8 +112,8 @@ def LSDecompFW(signal, width= 2**14,max_nnz_rate=8000/262144, sparsify = 0.01, t
     #GPSR
     ###############################
     nonzeros = float("Inf")
-    y =signal
-    c=Theta(y)
+    y = signal
+    c = signal
     maxabsThetaTy = max(abs(temp))
     
     
@@ -130,9 +122,9 @@ def LSDecompFW(signal, width= 2**14,max_nnz_rate=8000/262144, sparsify = 0.01, t
             tau = sparsify * maxabsThetaTy
             tolA = 1.0e-7
             
-            fh = linalg.LinearOperator(shape=temp2.shape,matvec=temp2,rmatvec=temp2.T,dtype=np.float64)
-               
-            c = relax.fista(y, fh, tolA, MaxiterA, c, tau)
+            fh = (temp,temp2.T)
+            print(fh)
+            c,r = relax.fista_scad(y, fh, tolA, MaxiterA, c, tau)
             
             #GPSR
             ###################
@@ -144,9 +136,10 @@ def LSDecompFW(signal, width= 2**14,max_nnz_rate=8000/262144, sparsify = 0.01, t
             if sparsify == 0.166:
                 sparsify = 0.1
                 
+    return             
     ###############################
     
     
 if __name__ == '__main__':
-    filepath = './080180500_5k'
-    ls_decomp_FW(filename = filepath)
+    filepath = '../../../080180500_5k'
+    run_LSdecompFW(filename = filepath)
